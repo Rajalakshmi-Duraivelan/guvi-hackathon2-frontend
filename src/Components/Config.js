ae1363cc3d85a3e89842c0712f266b5173dacc2d
Rajalakshmi Duraivelan <121893509+Rajalakshmi-Duraivelan@users.noreply.github.com>
2023-05-03 17:12:01 +0530

    Updated url

--- src/Components/Config.js
+++ src/Components/Config.js
@@ -1,4 +1,4 @@
 export const Config={
-    api:"http://localhost:5000"
-    //api:"https://equipment-rental-portal.onrender.com"
+    //api:"http://localhost:5000"
+    api:"https://equipment-rental-portall.onrender.com"
 }
